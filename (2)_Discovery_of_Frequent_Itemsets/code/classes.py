7deabaf222092576513fdd58c3d065c47612325c
Noel Karlsson Johansson <noelkj@kth.se>
2023-11-18 11:47:17 +0100

    Update

--- (2)_Discovery_of_Frequent_Itemsets/code/classes.py
+++ (2)_Discovery_of_Frequent_Itemsets/code/classes.py
@@ -1,36 +1,44 @@
 import pandas as pd
 from tqdm import tqdm
-
+import multiprocessing as mp
+import ast 
 class Dataset():
 
     def __init__(self, data_path: str, n: int):
         self.data_path = data_path
         self.n = n
-        self.df_data = self.load_dataset()
-        
+        self.data = self.load_dataset()
 
     def load_dataset(self):
         """
         Load a dataset from a .dat file.
         Assumes each line represents a transaction and items are separated by spaces.
         """
-        
+        dict_data = {}
+        count = 0
         with open(self.data_path, 'r') as file:
-            # Read only the first n lines
-            lines = file.readlines()[:self.n]
-            transactions = [line.strip() for line in lines]
+            for i, line in enumerate(file):
+                count += 1
+                transaction = [int(item) for item in line.strip().split(' ')]
+                for item in transaction:
+                    if item in dict_data.keys():
+                        dict_data[item].append(i)
+                    else: 
+                        dict_data[item] = [i]
+                if count == self.n:
+                    break
         
-        df = pd.DataFrame(transactions, columns=['Baskets'])
-
-        return df
+        
+        return dict_data
     
 from itertools import combinations
 
 class Apriori():
 
-    def __init__(self, baskets, min_support):
-        self.baskets = [basket.split() for basket in baskets.values]
+    def __init__(self, baskets, min_support, parallel=True):
+        self.baskets = baskets
         self.min_support = min_support
+        self.parallel = parallel
 
     def generate_candidates(self, baskets, k):
         """
@@ -39,54 +47,66 @@ class Apriori():
         candidates = set()
         
         for basket in tqdm(baskets, desc='Generating candidates for k={}'.format(k)):
-            candidates.update(combinations(basket, k))
+            candidates.update(combinations(baskets[basket], k))   
         return candidates
 
     def prune_candidates(self, candidates, prev_frequent_set, k):
         """
         Prune candidates that do not have all (k-1)-subsets in the previous frequent set.
         """
+        singleton_set = set()
+        for e in prev_frequent_set.keys():
+            singleton_set.update(e)
         pruned_candidates = set()
         for candidate in tqdm(candidates, desc='Pruning candidates for k={}'.format(k), position=0, leave=True):
             subsets = combinations(candidate, k-1)
-            if all(subset in prev_frequent_set for subset in subsets):
+            if all(subset in singleton_set for subset in subsets):
                 pruned_candidates.add(candidate)
         return pruned_candidates
+    
+    def count_candidates(self, candidate, baskets, s):
+        """
+        Count the occurrences of candidates in a list of baskets.
+        """
+        intersection = set.intersection(*[set(baskets[i]) for i in candidate])
+        if len(intersection) >= s:
+            return {candidate: len(intersection)}
+        return {}
 
 
     def apriori(self, baskets, min_support):
         """
         A-Priori algorithm for finding frequent itemsets.
         """
-        progress_bar = tqdm(desc='Main loop', leave=False)          # Progress bar
+        #progress_bar = tqdm(desc='Main loop', leave=False)          # Progress bar
         basket_count = len(baskets) # 100,000
         s = min_support * basket_count # 1000 = 0.01 * 100,000
 
-        # Initialize frequent itemsets
-        frequent_sets = []
         k = 1   # Size of itemsets
         candidates = self.generate_candidates(baskets, k)
+        print(candidates)
 
         while candidates:
             # Count occurrences of candidates in buckets
-            candidate_counts = {candidate: 0 for candidate in candidates}
-            for basket in tqdm(baskets, desc='Counting candidates for k={}'.format(k), leave=False):
-                for candidate in candidates:
-                    if set(candidate).issubset(basket):
-                        candidate_counts[candidate] += 1
+            if self.parallel:
+                pool = mp.Pool(mp.cpu_count())
+                result = pool.starmap(self.count_candidates, [(candidate, baskets, s) for candidate in candidates])
+                pool.close()
+            else:
+                result = [self.count_candidates(candidate, baskets, s) for candidate in candidates]
 
+            print(result)
             # Prune candidates that do not meet the minimum support
-            frequent_set = {candidate for candidate, count in candidate_counts.items() if count >= s}
-            frequent_sets.append({(candidate, count) for candidate, count in candidate_counts.items() if count >= s})
-            
+            frequent_set = {candidate: support for candidate_support in result for candidate, support in candidate_support.items()}
+            print(frequent_set)
             # Generate candidates for the next iteration
             k += 1
             candidates = self.generate_candidates(baskets, k)
             candidates = self.prune_candidates(candidates, frequent_set, k)
-            progress_bar.update()   # Update progress bar
-            progress_bar.refresh()  # Refresh progress bar
+            #progress_bar.update()   # Update progress bar
+            #progress_bar.refresh()  # Refresh progress bar
 
-        return frequent_sets
+        return frequent_set
     
     def __call__(self):
         frequent_sets = self.apriori(self.baskets, min_support=self.min_support)
