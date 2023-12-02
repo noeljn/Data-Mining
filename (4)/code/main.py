3c38383ba598bfcd665d3c2fdc7d198418f9e1d8
Noel Karlsson Johansson <noelkj@kth.se>
2023-12-02 14:18:08 +0100

    Update main.py

--- (4)/code/main.py
+++ (4)/code/main.py
@@ -4,24 +4,63 @@ from scipy.linalg import eigh
 import matplotlib.pyplot as plt
 import networkx as nx
 from classes import Graph
+from sklearn.cluster import KMeans
 
 # Create a graph object
+G = nx.read_edgelist('data/example1.dat', delimiter=',')
+nx.draw(G, node_size=15)
+plt.show()
+
+def spectral_clustering(G):
+    A = nx.adjacency_matrix(G)
+    D = np.diagflat(np.sum(A, axis=1))
+    D_inv = np.linalg.inv(D)
+    L = np.dot(np.dot(D_inv, A), D_inv)
+
+    eigenvalues, v = eigh(L)
+
+    # Get the k largest eigenvectors
+    k = np.ediff1d(eigenvalues).argmax() + 1
+    print(k)
+    X = v[:, -k:]
+    Y = X / np.linalg.norm(X, axis=1, keepdims=True)
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(Y)
+
+    _, fiedler_vector = eigh(D-A)
+    fiedler_vector = fiedler_vector[:, 1]
+
+    return kmeans.labels_, fiedler_vector, A
+
+#labels, fiedler_vector, A = spectral_clustering(G)
 G = Graph()
 
 G(filename='data/example1.dat')
 
 #G.visualize_graph()
-
+#
 G.number_of_clusters = 4
 
-# Construct the affinity matrix
-G.max_ids = np.max(G.nodes)
-G.A = np.zeros((G.max_ids, G.max_ids))
-for edge in G.edges:
-    G.A[edge[0]-1, edge[1]-1] = 1
+# Step 1. Construct the affinity matrix
+#G.max_ids = np.max(G.nodes)
+#G.A = np.zeros((G.max_ids, G.max_ids))
+print(len(G.edges))
+print(G.number_of_nodes())
+print(G.number_of_edges())
+for x in G.edges:
+    print(x)
+gamma = 1 # some constant - TODO: update this later!
+for i, edge in enumerate(G.edges): # Iterate over the edges
+    for j in range (len(G.edges)): #
+
+        pairwise_similarity = np.exp((-abs(G[i] - G[j]))**2) / (2 * gamma**2)
+        G.A[i][j] = pairwise_similarity
+    
+    #G.A[edge[0]-1, edge[1]-1] = 1
 
 # Create the affinity matrix. Holds the pairwise similarity between nodes using a Gaussian kernel.
 gamma = 1 # some constant - update this later!
+#for np.exp(-abs(G[i] -))
+
 #affinity_matrix = np.exp(-gamma * G.A)
 #affinity_matrix = np.exp(-gamma * np.square(np.linalg.norm(G[:, None] - G, axis=2)))
 
